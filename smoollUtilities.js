c082e1a85268a86b18fd3af0b7b0f41d9d9a755b
Jakub Skowron <jakubskowron676@gmail.com>
2024-09-09 19:09:07 +0200

    Test options menu

--- smoollUtilities.js
+++ smoollUtilities.js
@@ -1,21 +1,22 @@
 Game.registerMod("smoollUtilities", {
     // Custom functions
     optionsMenu: function() {
-        let str = "<div class=\"block\" style=\"padding:0px;margin:8px 4px;\">" +
-                    "<div class=\"subsection\" style=\"padding:0px\">" +
-                        "<div class=\"title\">" +
-                            "smoollUtilities" +
-                            "<div class=\"listing\">" +
-                                Game.WritePrefButton("neverCollapseUpgradeMenu", "ncumButton", "Never Collapse Upgrade Menu", "Never Collapse Upgrade Menu") +
-                                "<label>Open upgrade menu fully and never collapse it, even if cursor is not hovering over menu</label>" +
-                            "</div>" +
-                        "</div>" +
-                    "</div>" +
-               "<div/";
+        console.log("hello");
+        //let str = "<div class=\"block\" style=\"padding:0px;margin:8px 4px;\">" +
+        //            "<div class=\"subsection\" style=\"padding:0px\">" +
+        //                "<div class=\"title\">" +
+        //                    "smoollUtilities" +
+        //                    "<div class=\"listing\">" +
+        //                        Game.WritePrefButton("neverCollapseUpgradeMenu", "ncumButton", "Never Collapse Upgrade Menu", "Never Collapse Upgrade Menu") +
+        //                        "<label>Open upgrade menu fully and never collapse it, even if cursor is not hovering over menu</label>" +
+        //                    "</div>" +
+        //                "</div>" +
+        //            "</div>" +
+        //       "<div/";
 
-        if (Game.onMenu == "prefs") {
-            return str;
-        }
+        //if (Game.onMenu == "prefs") {
+        //    return str;
+        //}
     },
 
     // Functions needed for the mod to function
