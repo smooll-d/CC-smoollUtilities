137b374f6ed1f112c2acbe62ac850527e9a12157
Jakub Skowron <jakubskowron676@gmail.com>
2024-09-09 21:58:55 +0200

    Test options menu #45

--- smoollUtilities.js
+++ smoollUtilities.js
@@ -4,17 +4,20 @@ if (smoollUtilities === undefined) {
 
     var smoollUtilities = {
         optionsMenu: function() {
-            let str = `<div class="block" style="padding:0px;margin:8px 4px;">
-                            <div class="subsection" style="padding:0px">
-                                "<div class="title">
-                                    ${su}
-                                    <div class="listing">
-                                        ${Game.WritePrefButton("neverCollapseUpgradeMenu", "ncumButton", "Never Collapse Upgrade Menu", "Never Collapse Upgrade Menu")}
-                                        <label>Open upgrade menu fully and never collapse it, even if cursor is not hovering over menu</label>
-                                    </div>
-                                </div>
-                            </div>
-                      </div>`;
+            let str = `<div class="subsection" style="padding:0px">
+                           "<div class="title">
+                               ${su}
+                               <div class="listing">
+                                   ${Game.WritePrefButton("neverCollapseUpgradeMenu", "ncumButton", "Never Collapse Upgrade Menu", "Never Collapse Upgrade Menu")}
+                                   <label>Open upgrade menu fully and never collapse it, even if cursor is not hovering over menu</label>
+                               </div>
+                           </div>
+                       </div>`;
+
+            let menuDiv = document.createElement("div");
+            menuDiv.innerHTML = `<div class="block" style="padding:0px;margin:8px 4px;">
+                                    ${str}
+                                 </div>`;
 
             //let str = `<div class="block" style="padding:0px;margin:8px 4px;">
             //               <div class="subsection" style="padding:0px">
@@ -22,7 +25,10 @@ if (smoollUtilities === undefined) {
             //               </div>
             //           </div>`;
 
-            return str;
+            if (Game.ShowMenu()) {
+                menu.appendChild(menuDiv);
+                //menu.insertAdjacentHTML("afterend", smoollUtilities.optionsMenu());
+            }
         }
     };
 }
@@ -32,14 +38,9 @@ Game.registerMod(su, {
     init: function() {
         Game.Notify(su, "This \"smooll\" (get it? 'cause it's small) mod has been loaded!", [16, 5]);
 
-        let menuDiv = document.createElement("div");
-        menuDiv.innerHTML = smoollUtilities.optionsMenu();
+        Game.registerHook("logic", smoollUtilities.optionsMenu());
 
         //menu.insertAdjacentHTML("beforeend", smoollUtilities.optionsMenu());
-        if (Game.ShowMenu()) {
-            menu.appendChild(menuDiv);
-            //menu.insertAdjacentHTML("afterend", smoollUtilities.optionsMenu());
-        }
     },
 
     save: function() {
