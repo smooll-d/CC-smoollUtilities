4f19a05882e2c1c6d317d4b404f1401d0246f5a5
Jakub Skowron <jakubskowron676@gmail.com>
2024-09-09 19:41:42 +0200

    Test options menu

--- smoollUtilities.js
+++ smoollUtilities.js
@@ -22,7 +22,7 @@ Game.registerMod("smoollUtilities", {
     init: function() {
         Game.Notify("smoollUtilities", "This \"smooll\" (get it? 'cause it's small) mod has been loaded!", [16, 5]);
 
-        l("menu").innerHTML += smoollUtilities.optionsMenu();
+        l("menu").appendChild(smoollUtilities.optionsMenu());
     },
 
     save: function() {
