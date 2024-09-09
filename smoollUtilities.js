86ab702cb9f6456047998d6ce58b037213585b35
Jakub Skowron <jakubskowron676@gmail.com>
2024-09-09 19:26:43 +0200

    Test options menu

--- smoollUtilities.js
+++ smoollUtilities.js
@@ -1,36 +1,30 @@
 if (smoollUtilities === undefined) {
     var smoollUtilities = {
         optionsMenu: function() {
-            console.log("hello");
+            let str = "<div class=\"block\" style=\"padding:0px;margin:8px 4px;\">" +
+                            "<div class=\"subsection\" style=\"padding:0px\">" +
+                                "<div class=\"title\">" +
+                                    "smoollUtilities" +
+                                    "<div class=\"listing\">" +
+                                        Game.WritePrefButton("neverCollapseUpgradeMenu", "ncumButton", "Never Collapse Upgrade Menu", "Never Collapse Upgrade Menu") +
+                                        "<label>Open upgrade menu fully and never collapse it, even if cursor is not hovering over menu</label>" +
+                                    "</div>" +
+                                "</div>" +
+                            "</div>" +
+                      "<div/";
+
+            return str;
         }
     };
 }
 
 Game.registerMod("smoollUtilities", {
-    // Custom functions
-    //sUOptionsMenu: function() {
-    //    console.log("hello");
-        //let str = "<div class=\"block\" style=\"padding:0px;margin:8px 4px;\">" +
-        //            "<div class=\"subsection\" style=\"padding:0px\">" +
-        //                "<div class=\"title\">" +
-        //                    "smoollUtilities" +
-        //                    "<div class=\"listing\">" +
-        //                        Game.WritePrefButton("neverCollapseUpgradeMenu", "ncumButton", "Never Collapse Upgrade Menu", "Never Collapse Upgrade Menu") +
-        //                        "<label>Open upgrade menu fully and never collapse it, even if cursor is not hovering over menu</label>" +
-        //                    "</div>" +
-        //                "</div>" +
-        //            "</div>" +
-        //       "<div/";
-
-        //if (Game.onMenu == "prefs") {
-        //    return str;
-        //}
-    //},
-
-    // Functions needed for the mod to function
     init: function() {
         Game.Notify("smoollUtilities", "This \"smooll\" (get it? 'cause it's small) mod has been loaded!", [16, 5]);
-        smoollUtilities.optionsMenu();
+
+        if (Game.onMenu == "prefs") {
+            smoollUtilities.optionsMenu();
+        }
     },
 
     save: function() {
