d5c699e56c247eb806a34daf8c15b0f16be7d0b6
Jakub Skowron <jakubskowron676@gmail.com>
2024-09-09 18:30:02 +0200

    Test options menu

--- smoollUtilities.js
+++ smoollUtilities.js
@@ -1,7 +1,7 @@
 Game.registerMod("smoollUtilities", {
     init:function() {
-        Game.Notify("smoollUtilities", "This \"smooll\" (get it? 'cause it's small) mod has been loaded!", [16, 5])
-        Game.WritePrefButton("test1", "ncum", "testON", "testOFF");
+        Game.Notify("smoollUtilities", "This \"smooll\" (get it? 'cause it's small) mod has been loaded!", [16, 5]);
+        optionsMenu();
     },
 
     save:function() {
@@ -10,10 +10,20 @@ Game.registerMod("smoollUtilities", {
 
     load:function(str) {
         
-    }
+    },
 
     // Custom functions
-    //optionsMenu:function() {
-    //    return 
-    //}
+    optionsMenu:function() {
+        return "<div class=\"block\" style=\"padding:0px;margin:8px 4px;\">" +
+                    "<div class=\"subsection\" style=\"padding:0px\">" +
+                        "<div class=\"title\">" +
+                            "smoollUtilities" +
+                            "<div class=\"listing\">" +
+                                Game.WritePrefButton("test1", "ncum", "testON", "testOFF") +
+                                "<label>Open upgrade menu fully and never collapse it, even if cursor is not hovering over menu</label>" +
+                            "</div>" +
+                        "</div>" +
+                    "</div>" +
+               "<div/";
+    }
 });
