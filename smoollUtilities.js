735ccf4f08046c092e35d33125677101136e6f6f
Jakub Skowron <jakubskowron676@gmail.com>
2024-09-10 13:55:34 +0200

    Test options menu #82

--- smoollUtilities.js
+++ smoollUtilities.js
@@ -1,57 +1,55 @@
 if (smoollUtilities === undefined) {
     var su = "smoollUtilities";
     var menu = l("menu");
-
-    var smoollUtilities = {
-        optionsMenu: function() {
-            let html = `<div class="block" style="padding:0px;margin:8px 4px;">
-                           <div class="subsection" style="padding:0px">
-                               <div class="title">
-                                   ${su}
-                                   <div class="listing">
-                                       ${Game.WritePrefButton("neverCollapseUpgradeMenu", "ncumButton", "Never Collapse Upgrade Menu", "Never Collapse Upgrade Menu")}
-                                       <label>Open upgrade menu fully and never collapse it, even if cursor is not hovering over menu</label>
-                                   </div>
-                               </div>
-                           </div>
-                       </div>`;
-
-            // let div = document.createElement("div");
-            // div.innerHTML = html;
-
-            // menu.appendChild(div);
-
-            if (menu) {
-                menu.insertAdjacentHTML("beforeend", html);
-            }
-
-            Game.UpdateMenu();
-
-            console.log("Hello, this is an options menu test");
-
-            // let menuDiv = document.createElement("div");
-            // menuDiv.innerHTML = `<div class="block" style="padding:0px;margin:8px 4px;">
-            //                         ${str}
-            //                      </div>`;
-
-            // if (menu) {
-            //     menu.appendChild(menuDiv);
-            // }
-
-            //let str = `<div class="block" style="padding:0px;margin:8px 4px;">
-            //               <div class="subsection" style="padding:0px">
-            //                   ${this.collapsibleMenu("smoollUtilities", "Hello, test!")}
-            //               </div>
-            //           </div>`;
-
-            //menu.insertAdjacentHTML("afterend", smoollUtilities.optionsMenu());
-        }
-    };
 }
 
 Game.registerMod(su, {
+    optionsMenu: function() {
+        let html = `<div class="block" style="padding:0px;margin:8px 4px;">
+                       <div class="subsection" style="padding:0px">
+                           <div class="title">
+                               ${su}
+                               <div class="listing">
+                                   ${Game.WritePrefButton("neverCollapseUpgradeMenu", "ncumButton", "Never Collapse Upgrade Menu", "Never Collapse Upgrade Menu")}
+                                   <label>Open upgrade menu fully and never collapse it, even if cursor is not hovering over menu</label>
+                               </div>
+                           </div>
+                       </div>
+                   </div>`;
+
+        // let div = document.createElement("div");
+        // div.innerHTML = html;
+
+        // menu.appendChild(div);
+
+        if (menu) {
+            menu.insertAdjacentHTML("beforeend", html);
+        }
+
+        Game.UpdateMenu();
+
+        console.log("Hello, this is an options menu test");
+
+        // let menuDiv = document.createElement("div");
+        // menuDiv.innerHTML = `<div class="block" style="padding:0px;margin:8px 4px;">
+        //                         ${str}
+        //                      </div>`;
+
+        // if (menu) {
+        //     menu.appendChild(menuDiv);
+        // }
+
+        //let str = `<div class="block" style="padding:0px;margin:8px 4px;">
+        //               <div class="subsection" style="padding:0px">
+        //                   ${this.collapsibleMenu("smoollUtilities", "Hello, test!")}
+        //               </div>
+        //           </div>`;
+
+        //menu.insertAdjacentHTML("afterend", smoollUtilities.optionsMenu());
+    },
+
     init: function() {
-        Game.registerHook("check", smoollUtilities.optionsMenu);
+        Game.registerHook("check", this.optionsMenu);
 
         Game.Notify(su, "This \"smooll\" (get it? 'cause it's small) mod has been loaded!", [16, 5]);
 
