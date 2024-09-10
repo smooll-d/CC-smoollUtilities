a23e9da9f721f0d1001ffc4962981e19dd4d8e79
Jakub Skowron <jakubskowron676@gmail.com>
2024-09-10 23:57:19 +0200

    further customization of `optionsMenu()`

--- smoollUtilities.js
+++ smoollUtilities.js
@@ -1,5 +1,4 @@
 var menu = document.getElementById("menu");
-var general = document.getElementById("statsGeneral");
 
 if (smoollUtilities === undefined) {
     var smoollUtilities = {
@@ -17,7 +16,7 @@ if (smoollUtilities === undefined) {
                 smoollUtilities.statsMenu();
             };
 
-            Game.Notify(smoollUtilities.name, "This \"smooll\" (get it? 'cause it's small) mod has been loaded!", [16, 5]);
+            Game.Notify(smoollUtilities.name, "This \"smooll\" (get it? 'cause it's small) mod has been loaded!", [16, 5], 3);
         },
 
         save: function() {
@@ -35,13 +34,13 @@ if (smoollUtilities === undefined) {
                 div.className = "block";
                 div.style.padding = "0px";
                 div.style.margin = "8px 4px";
-                div.innerHTML = `<div class="subsection" style="padding:0px">
+                div.innerHTML = `<div class="subsection" style="padding:0px;">
                                      <div class="title">
                                          ${this.name}
-                                         <div class="listing">
-                                             ${Game.WritePrefButton("neverCollapseUpgradeMenu", "ncumButton", "Never Collapse Upgrade Menu", "Never Collapse Upgrade Menu")}
-                                             <label>Open upgrade menu fully and never collapse it, even if cursor is not hovering over menu</label>
-                                         </div>
+                                     </div>
+                                     <div class="listing">
+                                         ${Game.WritePrefButton("neverCollapseUpgradeMenu", "ncumButton", "Never Collapse Upgrade Menu", "Never Collapse Upgrade Menu")}
+                                         <label>Open upgrade menu fully and never collapse it, even if cursor is not hovering over menu</label>
                                      </div>
                                 </div>`;
 
